ad0fcf3a9d4816e3d630ada4846f7ae1a2c9e8c5
Akshay Dixit <akshaydixi@gmail.com>
2013-12-10 11:30:33 +0530

    added monitor control

--- main.c
+++ main.c
@@ -1,4 +1,21 @@
+#include "monitor.h"
+
 int main(struct multiboot *mboot_ptr)
 {
-  return 0xDEADBABA;
+
+  monitor_clear();
+  monitor_write("Hello, World!\r\n");
+  monitor_write("This is a test\r");
+   monitor_write("This is a test\r\n");
+  monitor_write("This is a test\r");
+  monitor_write("This is a test\r\n");
+  monitor_write("This is a test\r");
+  monitor_write("This is a test\r\n");
+  monitor_write("This is a test\r");
+  monitor_write("This is a test\r\n");
+  monitor_write("This is a test\r");
+  monitor_write("This is a test\r\n");
+  monitor_write("This is a test\r");
+  
+   return 0;
 }
